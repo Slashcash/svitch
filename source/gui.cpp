3326f029c3b7547895102f624dfabc5f06ff2960
gennaro <gen.iorio92@gmail.com>
2018-06-01 23:32:25 +0200

    add italian language support

--- source/gui.cpp
+++ source/gui.cpp
@@ -17,6 +17,7 @@ Gui::Gui() {
 
     //all the language supported by this software (add them here to support more of them)
     supported_languages.push_back("en-US");
+    supported_languages.push_back("it");
 
     #ifndef EMULATOR
     //getting the system language
